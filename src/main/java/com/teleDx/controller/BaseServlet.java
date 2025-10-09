2ebf89b659265c4822f7ee812efd8be1a2357bc6
Anouar El barry <elbarryanwar37@gmail.com>
2025-10-09 11:17:56 +0100

    TEM-13 feat:create base servlet

--- src/main/java/com/teleDx/controller/BaseServlet.java
+++ src/main/java/com/teleDx/controller/BaseServlet.java
@@ -1,4 +1,22 @@
 package com.teleDx.controller;
 
-public class BaseServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "BaseServlet",urlPatterns = {"/"})
+public class BaseServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException , IOException {
+        req.getRequestDispatcher("/index.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws  ServletException , IOException{
+        doGet(req,resp);
+    }
 }

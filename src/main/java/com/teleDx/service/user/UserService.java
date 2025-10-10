f963edaa30cc7c9dd447d66ea974d45dedd58e79
Anouar El barry <elbarryanwar37@gmail.com>
2025-10-10 16:26:03 +0100

    feat:complete  wor th user

--- src/main/java/com/teleDx/service/user/UserService.java
+++ src/main/java/com/teleDx/service/user/UserService.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
-    Optional<User> findById(Long id);
+    Optional<User> findById(String id);
+    Optional<User> findByUsername(String username);
     List<User> findAll();
     User save(User user);
-    void deleteById(Long id);
+    boolean deleteById(String id);
+    boolean authenticate(String username, String password);
 }
--- src/main/java/com/teleDx/service/user/UserServiceImpl.java
+++ src/main/java/com/teleDx/service/user/UserServiceImpl.java
@@ -1,5 +1,55 @@
 package com.teleDx.service.user;
 
-public class UserServiceImpl {
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.teleDx.dao.User.UserDAOImpl;
+import com.teleDx.entity.User;
+import jakarta.inject.Inject;
 
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService{
+
+    @Inject
+    private UserDAOImpl userDAO ;
+
+    @Override
+    public Optional<User> findById(String id) {
+        return userDAO.findById(id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userDAO.findAll();
+    }
+
+    @Override
+    public User save(User user) {
+        if (user.getPassword() != null){
+            String hashedPassword = BCrypt.withDefaults().hashToString(12,user.getPassword().toCharArray());
+            user.setPassword(hashedPassword);
+        }
+        return userDAO.save(user);
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return userDAO.deletById(id);
+    }
+
+    @Override
+    public boolean authenticate(String username, String password){
+        Optional<User> userOpt = userDAO.findByUsername(username);
+        if(userOpt.isPresent()){
+            User user = userOpt.get();
+            BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(),user.getPassword());
+            return result.verified;
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username){
+       return userDAO.findByUsername(username);
+    }
 }

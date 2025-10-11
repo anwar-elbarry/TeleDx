a1c38e12a604ae6e212338d10f7c3e77d450a8c7
Anouar El barry <elbarryanwar37@gmail.com>
2025-10-11 14:24:51 +0100

    feat:create role service

--- src/main/java/com/teleDx/service/role/RoleService.java
+++ src/main/java/com/teleDx/service/role/RoleService.java
@@ -1,4 +1,9 @@
 package com.teleDx.service.role;
 
+import com.teleDx.entity.Enums.RoleName;
+import com.teleDx.entity.Role;
+
 public interface RoleService {
+    Role save(String name);
+    Role findByName(RoleName name);
 }
--- src/main/java/com/teleDx/service/role/RoleServiceImpl.java
+++ src/main/java/com/teleDx/service/role/RoleServiceImpl.java
@@ -1,4 +1,22 @@
 package com.teleDx.service.role;
 
-public class RoleServiceImpl {
+import com.teleDx.dao.role.RoleDAOImpl;
+import com.teleDx.entity.Enums.RoleName;
+import com.teleDx.entity.Role;
+
+public class RoleServiceImpl implements RoleService{
+    private final RoleDAOImpl roleDAOImpl;
+
+    public RoleServiceImpl(RoleDAOImpl roleDAOImpl) {
+        this.roleDAOImpl = roleDAOImpl;
+    }
+    @Override
+    public Role save(String name) {
+        return roleDAOImpl.save(name);
+    }
+
+    @Override
+    public Role findByName(RoleName name) {
+        return roleDAOImpl.findByName(name);
+    }
 }

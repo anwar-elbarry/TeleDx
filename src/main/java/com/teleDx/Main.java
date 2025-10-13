bb839446bbd6487090bbf6f6778f6934ae46f63d
Anouar El barry <elbarryanwar37@gmail.com>
2025-10-13 14:40:34 +0100

    commit before merge

--- src/main/java/com/teleDx/Main.java
+++ src/main/java/com/teleDx/Main.java
@@ -1,14 +1,5 @@
 package com.teleDx;
 
-import com.teleDx.entity.Enums.RoleName;
-import com.teleDx.entity.Role;
-import com.teleDx.util.JpaUtil;
-import jakarta.persistence.EntityManager;
-
-import javax.swing.*;
-import java.time.LocalDate;
-import java.util.UUID;
-
 public class Main {
     public static void main(String[] args) {
     }
